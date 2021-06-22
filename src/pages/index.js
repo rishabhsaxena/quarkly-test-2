c462d01482f7e553c17d0bda1c27502a7781c09b
Rishabh Saxena <rishabh.robben@gmail.com>
2021-06-22 18:34:09 +0530

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ export default (() => {
 					text-transform="uppercase"
 					margin="6px 0"
 				>
-					About me
+					sdfas dfas d
 				</Text>
 				<Text as="h2" font="--headline2" margin="0 0 12px 0">
 					Hey I’m David
