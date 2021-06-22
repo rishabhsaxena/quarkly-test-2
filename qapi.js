e0b07707016670fc3bc42742bea9bd6aec6b30ea
Rishabh Saxena <rishabh.robben@gmail.com>
2021-06-22 18:33:18 +0530

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60d1df20055903001fd692e8",
+				"60d1df20055903001fd692ea"
+			]
+		},
+		"60d1df20055903001fd692e8": {
+			"id": "60d1df20055903001fd692e8",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60d1df20055903001fd692ea": {
+			"id": "60d1df20055903001fd692ea",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
